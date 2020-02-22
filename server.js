40c282b64eac42d518c056eaed0f5688384cb960
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-02-22 10:46:55 -0800

    heroku password test

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.static("public"));
 
 var DB_URL = process.env.MONGODB_URI;
 // Connect to the Mongo DB
-mongoose.connect("mongodb://miguel:newsscraper1@ds359847.mlab.com:59847/heroku_w59t3d2v", { useNewUrlParser: true });
+mongoose.connect(DB_URL, { useNewUrlParser: true });
 // Routes
 
 // main page 
